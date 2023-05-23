f241d71f45a9a05276e6a5bfd374f4b4a633a7fd
Radu Ifrim <radu.ifrim@xoserveazure.onmicrosoft.com>
2023-05-23 14:18:48 +0100

    update formulae to latest version

--- Formula/gcta.rb
+++ Formula/gcta.rb
@@ -1,17 +1,17 @@
 class Gcta < Formula
     desc "Go Code Test Analyzer"
     homepage "https://github.com/radurobot/go-code-test-analyzer"
-    version "0.1.0"
-  
+    version "0.1.1"
+    
     if OS.linux?
-      url "https://github.com/radurobot/go-code-test-analyzer/releases/download/v0.1.0/gcta-linux-amd64", using: :nounzip
-      sha256 "c0d993b2884c61811deae2c40e4fbc8a44fb27bb4bc93e2e8189bf29eed4e8a2"
+      url "https://github.com/radurobot/go-code-test-analyzer/releases/download/v0.1.1/gcta-linux-amd64", using: :nounzip
+      sha256 "1ee4765034efc74bcf93998152e8d9e755b65f46c18801b21104cb4ff54ed6ba"
     elsif OS.mac? && Hardware::CPU.arm?
-      url "https://github.com/radurobot/go-code-test-analyzer/releases/download/v0.1.0/gcta-darwin-arm64", using: :nounzip
-      sha256 "d33396b846056f7cfb89cd6b804436981b9b36db50eed87ad2662bfd9842c55b"
+      url "https://github.com/radurobot/go-code-test-analyzer/releases/download/v0.1.1/gcta-darwin-arm64", using: :nounzip
+      sha256 "d541d8dff74211e572c89a417bafc7aa5f60d3d2cb13a0a3837173ab80207902"
     elsif OS.mac? && Hardware::CPU.intel?
-      url "https://github.com/radurobot/go-code-test-analyzer/releases/download/v0.1.0/gcta-darwin-amd64", using: :nounzip
-      sha256 "ad128e26c41520e85cbbb052e478ef9279301c975935ca17fa2d8ccb505fc380"
+      url "https://github.com/radurobot/go-code-test-analyzer/releases/download/v0.1.1/gcta-darwin-amd64", using: :nounzip
+      sha256 "284533cdee753e7c5d8a9de24e73cec120f2251ce94ab8afbd18bbf68246e8b5"
     end
   
     def install
